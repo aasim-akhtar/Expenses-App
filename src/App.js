d76642b681cfdb1a4c85de08f6aef5d197c241f5
aasim-akhtar <xpaasim@gmail.com>
2021-10-20 15:21:49 +0530

    added add new Expenses to list functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
-const App = () => {
-  const expenses = [
+
+
+  const DUMMY_EXPENSES = [
     {
       id: 'e1',
       title: 'Toilet Paper',
@@ -24,6 +25,10 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+const App = () => {
+  const [expenses,setExpenses] = useState(DUMMY_EXPENSES);
+  
+
 
   // return React.createElement(
   //   'div',
@@ -33,17 +38,21 @@ const App = () => {
   // );
 
   // bringing data from new expense
-  const addExpenseDataHandler = (data) => {
-    const newExpenseData = data; 
-    console.log(newExpenseData);
+  // const addExpenseDataHandler = (data) => {
+  //   const newExpenseData = data;
+  //   console.log(newExpenseData);
+  // };
+  const addExpenseDataHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   };
-
   return (
     <div>
       <NewExpense onAddExpense={addExpenseDataHandler} />
-      <Expenses items={expenses} /> 
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
