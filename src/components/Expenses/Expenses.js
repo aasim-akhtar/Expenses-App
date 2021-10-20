bef419ca3b014f4f862f3af72ba87161325407fa
aasim-akhtar <xpaasim@gmail.com>
2021-10-20 15:46:56 +0530

    ternary expression changed to && standalone expression

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -23,17 +23,17 @@ const Expenses = (props) => {
         onChangeFilter={filterChangeHandler}
       />
       {/* Long statements like if and for do not work in dynamic expession, use terniary expressions instead*/}
-      {filteredExpenses.length === 0 ? ( 
-       <p>No Expenses Found.</p>) : ( 
-         filteredExpenses.map((expense) => (
+      {filteredExpenses.length === 0 && <p>No Expenses Found.</p>}
+{/* && renders text whenever the first condition is true */}
+      {filteredExpenses.length > 0 &&
+        filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
         />
-      ))
-      )}
+      ))}
     </Card>
   );
 }
